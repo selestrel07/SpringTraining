2120db3ab10f580109da590f2c2b2c8e4b899e86
selestrel07 <selestrel07@gmail.com>
2023-11-16 18:14:26 +0400

    Lesson 7: Dependency Injection Part 2

--- src/main/java/ru/kulevich/springcourse/MusicPlayer.java
+++ src/main/java/ru/kulevich/springcourse/MusicPlayer.java
@@ -1,14 +1,37 @@
 package ru.kulevich.springcourse;
 
+import java.util.List;
+
 public class MusicPlayer {
 
-    private Music music;
+    private List<Music> musicList;
+    private String name;
+    private int volume;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer() {
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        musicList.forEach(c -> System.out.println("Playing: " + c.getSong()));
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
---
+++ src/main/java/ru/kulevich/springcourse/PopMusic.java
@@ -0,0 +1,9 @@
+package ru.kulevich.springcourse;
+
+public class PopMusic implements Music {
+
+    @Override
+    public String getSong() {
+        return "Oops! I did it again!";
+    }
+}
